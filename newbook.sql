0d801ba340df05b2dc7dbecd09cc307b4ca2d7c1
pussbb <pussbb@gmail.com>
2011-04-06 20:42:55 +0300

    remove old from master

--- newbook.sql
+++
@@ -1,7 +0,0 @@
-     QSqlQuery query;
-     query.exec("create table general ("
-                "bokname text, descr text,hash text,articles int)");
-     query.exec("create table bookcat (id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                "name varchar(20), descr text,parent int default 0, subcat int default 0)");
-     query.exec("create table articles (id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                "name text, body text,author varchar(50),pdate DATE,md5 text,catid int)");
